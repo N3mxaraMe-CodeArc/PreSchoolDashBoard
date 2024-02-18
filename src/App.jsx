f865c2b830441e1f094c40adae61c99193e98125
N3mxaraMe-CodeArc <nimsaraofficial@gmail.com>
2024-02-18 10:48:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/NavBar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg mt-2" data-bs-theme="light">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">
+          Brand Name
+        </a>
+        <div className="d-flex justify-content-end align-items-center">
+          <button className="btn btn-outline-light mx-2 fw-bold">Login</button>
+          <button
+            style={{ backgroundColor: "#8D5CF6" }}
+            className="btn text-white joinUs"
+          >
+            Join us
+            <i
+              style={{ background: "none", marginLeft: "10px" }}
+              className="bi bi-arrow-right"
+            ></i>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
